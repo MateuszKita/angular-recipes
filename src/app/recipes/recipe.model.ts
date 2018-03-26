05c1fc13a897cd6b9899889f67350bf3d2225cb0
Mateusz Kita <mateusz.kita@globallogic.com>
2018-03-26 14:26:19 +0200

    preparation for displaying recipe details

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,11 @@
 export class Recipe {
   public name: string;
   public description: string;
-  public imagePath: string;
+  public imgPath: string;
 
-  constructor(name: string, description: string, imagePath: string) {
+  constructor(name: string, description: string, imgPath: string) {
     this.name = name;
     this.description = description;
-    this.imagePath = imagePath;
+    this.imgPath = imgPath;
   }
 }
