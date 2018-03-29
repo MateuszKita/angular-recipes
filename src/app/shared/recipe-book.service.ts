bff00695cf592aba23e21c4d00cc901e635ebaa8
Mateusz Kita <mateusz.kita@globallogic.com>
2018-03-29 15:28:53 +0200

    adding ingredients to recipes

--- src/app/shared/recipe-book.service.ts
+++ src/app/shared/recipe-book.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from './ingredient.model';
 
 @Injectable()
 export class RecipeBookService {
@@ -11,12 +12,14 @@ export class RecipeBookService {
     new Recipe(
       'Pancakes',
       'Tasty pancakes',
-      'https://upload.wikimedia.org/wikipedia/commons/a/aa/Blueberry_pancakes_%281%29.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/a/aa/Blueberry_pancakes_%281%29.jpg',
+      [new Ingredient('Milk', 1), new Ingredient('Flour', 2)]
     ),
     new Recipe(
       'Donuts',
       'Tasty donuts',
-      'https://upload.wikimedia.org/wikipedia/commons/1/1c/Donuts.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/1/1c/Donuts.jpg',
+      [new Ingredient('Jam', 2), new Ingredient('Oil', 3)]
     )
   ];
 
--- src/app/shared/recipe.model.ts
+++ src/app/shared/recipe.model.ts
@@ -1,7 +1,10 @@
+import { Ingredient } from './ingredient.model';
+
 export class Recipe {
   constructor(
     public name: string,
     public description: string,
-    public imgPath: string
+    public imgPath: string,
+    public ingredients: Ingredient[]
   ) {}
 }
